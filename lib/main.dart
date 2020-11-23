4814fe4ea24a9dc1bc0a36c3a82d1a34f7f76e74
Khoa Vo <shanghaicityboi@gmail.com>
2020-11-23 15:05:58 +0700

    Integrate Facebook Login into Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:unclechat_v1/screen/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(UncleChat());
 }
 
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class LoginScreen extends StatelessWidget {
+  final facebookAuth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +17,9 @@ class LoginScreen extends StatelessWidget {
           children: [
             RaisedButton(
               onPressed: () async {
-                final result = await FacebookAuth.instance.login();
-                final token = result.token;
-                if (token != null) {
-                  final userData = await FacebookAuth.instance.getUserData();
-                  print(userData.entries);
-                }
+                final user = await loginWithFacebook();
+                print(user.user.uid);
+                print(user.user.providerData);
               },
               child: Text('Login with Facebook'),
             ),
@@ -27,5 +27,12 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<UserCredential> loginWithFacebook() async {
+    final result = await FacebookAuth.instance.login();
+    final token = result.token;
+    final credential = FacebookAuthProvider.credential(token);
+    return facebookAuth.signInWithCredential(credential);
   }
 }
