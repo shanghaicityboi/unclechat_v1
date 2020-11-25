198417fbfbdcacb463061dd1c5d4fa45caa55187
Khoa Vo <shanghaicityboi@gmail.com>
2020-11-25 19:56:45 +0700

    Improve UI for Info Form in Profile Screen

--- lib/form/info.dart
+++ lib/form/info.dart
@@ -16,94 +16,130 @@ class InfoForm extends StatelessWidget {
           key: _formKey,
           child: Column(
             children: [
-              FormBuilderTextField(
-                name: 'name',
-                decoration: InputDecoration(
-                  labelText: 'Nickname',
-                ),
-                validator: FormBuilderValidators.compose([
-                  FormBuilderValidators.required(context),
-                  FormBuilderValidators.minLength(context, 6),
-                  FormBuilderValidators.maxLength(context, 16),
-                ]),
-                keyboardType: TextInputType.number,
-              ),
-              FormBuilderTextField(
-                name: 'year',
-                decoration: InputDecoration(
-                  labelText: 'Birthyear',
-                ),
-                validator: FormBuilderValidators.compose([
-                  FormBuilderValidators.required(context),
-                  FormBuilderValidators.min(context, _thisYear - 45),
-                  FormBuilderValidators.max(context, _thisYear - 13),
-                ]),
-                keyboardType: TextInputType.number,
-              ),
-              FormBuilderDropdown(
-                name: 'gender',
-                decoration: InputDecoration(
-                  labelText: 'Gender',
-                ),
-                allowClear: true,
-                hint: Text('Select Gender'),
-                validator: FormBuilderValidators.compose(
-                    [FormBuilderValidators.required(context)]),
-                items: _genders.map((gender) {
-                  return DropdownMenuItem(value: gender, child: Text(gender));
-                }).toList(),
-              ),
-              FormBuilderDropdown(
-                name: 'region',
-                decoration: InputDecoration(
-                  labelText: 'Region',
-                ),
-                allowClear: true,
-                hint: Text('Select Region'),
-                validator: FormBuilderValidators.compose(
-                    [FormBuilderValidators.required(context)]),
-                items: _regions.map((region) {
-                  return DropdownMenuItem(value: region, child: Text(region));
-                }).toList(),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: MaterialButton(
-                      color: Theme.of(context).accentColor,
-                      child: Text(
-                        "Submit",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        _formKey.currentState.save();
-                        if (_formKey.currentState.validate()) {
-                          print(_formKey.currentState.value);
-                        } else {
-                          print("validation failed");
-                        }
-                      },
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: MaterialButton(
-                      color: Theme.of(context).accentColor,
-                      child: Text(
-                        "Reset",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        _formKey.currentState.reset();
-                      },
-                    ),
-                  ),
-                ],
-              ),
+              _buildNameField(context),
+              _buildYearField(context),
+              _buildGenderField(context),
+              _buildRegionField(context),
+              _buildFormButtons(context),
             ],
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildNameField(BuildContext context) {
+    return FormBuilderTextField(
+      name: 'name',
+      decoration: InputDecoration(
+        labelText: 'Nickname',
+        filled: true,
+        hintText: 'Fill your nickname',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+      ),
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(context),
+        FormBuilderValidators.minLength(context, 6),
+        FormBuilderValidators.maxLength(context, 16),
+      ]),
+      keyboardType: TextInputType.number,
+    );
+  }
+
+  Widget _buildYearField(BuildContext context) {
+    return FormBuilderTextField(
+      name: 'year',
+      decoration: InputDecoration(
+        labelText: 'Birthyear',
+        filled: true,
+        hintText: 'Fill your birthyear',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+      ),
+      validator: FormBuilderValidators.compose([
+        FormBuilderValidators.required(context),
+        FormBuilderValidators.min(context, _thisYear - 45),
+        FormBuilderValidators.max(context, _thisYear - 13),
+      ]),
+      keyboardType: TextInputType.number,
+    );
+  }
+
+  Widget _buildGenderField(BuildContext context) {
+    return FormBuilderDropdown(
+      name: 'gender',
+      decoration: InputDecoration(
+        labelText: 'Gender',
+        filled: true,
+      ),
+      allowClear: true,
+      hint: Text('Select Gender'),
+      validator: FormBuilderValidators.compose(
+          [FormBuilderValidators.required(context)]),
+      items: _genders.map((gender) {
+        return DropdownMenuItem(value: gender, child: Text(gender));
+      }).toList(),
+    );
+  }
+
+  Widget _buildRegionField(BuildContext context) {
+    return FormBuilderDropdown(
+      name: 'region',
+      decoration: InputDecoration(
+        labelText: 'Region',
+        filled: true,
+      ),
+      allowClear: true,
+      hint: Text('Select Region'),
+      validator: FormBuilderValidators.compose(
+          [FormBuilderValidators.required(context)]),
+      items: _regions.map((region) {
+        return DropdownMenuItem(value: region, child: Text(region));
+      }).toList(),
+    );
+  }
+
+  Widget _buildFormButtons(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        _buildSubmitButton(context),
+        SizedBox(width: 5),
+        _buildResetButton(context),
+      ],
+    );
+  }
+
+  Widget _buildSubmitButton(BuildContext context) {
+    return Expanded(
+      child: MaterialButton(
+        color: Theme.of(context).accentColor,
+        child: Text(
+          "Submit",
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          _formKey.currentState.save();
+          if (_formKey.currentState.validate()) {
+            print(_formKey.currentState.value);
+          } else {
+            print("validation failed");
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildResetButton(BuildContext context) {
+    return Expanded(
+      child: MaterialButton(
+        color: Theme.of(context).accentColor,
+        child: Text(
+          "Reset",
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          _formKey.currentState.reset();
+        },
+      ),
     );
   }
 }
